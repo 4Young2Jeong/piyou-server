28e1b62408bc9bd07e92ab7f6c7d48d65571eac1
인영교 <yeongkyo1997@gmail.com>
2023-11-16 11:21:40 +0900

    Add GET endpoint for testing scheduler

--- backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/controller/SchedulerController.java
+++ backend/spring-server/src/main/java/com/b106_402jeoung/PIYOU/controller/SchedulerController.java
@@ -3,6 +3,7 @@ package com.b106_402jeoung.PIYOU.controller;
 import com.b106_402jeoung.PIYOU.service.SchedulerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class SchedulerController {
      * isMeal이 false인 아이의 경험치를 4 감소, scheduler를 사용해서 매일 00:00:00에 실행
      */
     @Scheduled(cron = "0 0 0 * * *")
+    @GetMapping("/test")
     public void updateChildExp() {
         schedulerService.updateChildExp();
     }
